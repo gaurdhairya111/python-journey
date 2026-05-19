a8b8ee807578acfa46ae8e4b356f46622cbb8215
gaurdhairya111 <gaur.dhairya111@gmail.com>
2026-05-19 22:22:08 +0530

    adding a temprature converter

---
+++ week_1/temprature_converter.py
@@ -0,0 +1,13 @@
+#this program is to convert temprature from celcius to Fahrenheit
+"""basic idea is to take input from user and 
+convert it into fahrenheit to calculate the temprature"""
+
+celcius = float(input("Enter the current temprature : "))
+
+c = celcius
+fahrenheit =  "f"
+
+f = (c*1.8)+32
+
+print("the temprature",celcius,"C","in fahrenheiht is :",f,"F")
+
