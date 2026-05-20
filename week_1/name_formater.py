57f8d596be706585e5d12057dc6a2a09491942e7
gaurdhairya111 <gaur.dhairya111@gmail.com>
2026-05-20 22:36:41 +0530

    this is my name formater

---
+++ week_1/name_formater.py
@@ -0,0 +1,24 @@
+# NAME VALIDATOR
+# USING BASIC STRING OPERATION TO MAKE THIS
+
+print("="*50)
+x = str(input("Enter your full name here : "))
+print("="*50)
+
+#upper case
+print(x.upper())
+
+#lower case 
+print(x.lower())
+
+#tittle case
+print(x.title())
+
+#capitalize
+print(x.capitalize())
+
+#reverse of the name
+print(x[::-1])
+
+
+
