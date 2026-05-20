b3a1b9ed67ceeab275e3280852b110534b154b53
gaurdhairya111 <gaur.dhairya111@gmail.com>
2026-05-20 23:07:24 +0530

    this is a simple text replacer

---
+++ week_1/text_replacer.py
@@ -0,0 +1,40 @@
+#TEXT REPLACER
+#USING BASIC STRING OPERATION {replace} TO PERFORM IT
+
+s1 = "This is my sixth PYTHON program, and till now everything is going smooth in PYTHON from my side and I am having fun in learning PYTHON this way" 
+
+print("-"*60)
+print("-"*60)
+
+print(f"\n this is the original string: {s1}\n")
+print("-"*60)
+
+print("-"*60,"\n")
+
+print(s1.replace("PYTHON","JAVA"))
+
+print("-"*60)
+print("-"*60,"\n")
+
+print(s1.replace("PYTHON","JAVA",1))
+print("-"*60)
+print("-"*60)
+
+print(s1.upper())
+print("-"*60)
+print("-"*60)
+
+
+print(s1.lower())
+print("-"*60)
+print("-"*60)
+
+
+print(s1[::-1])
+print("-"*60)
+print("-"*60)
+
+
+
+
+
