dcd5c200e7a2c998222c0bc2e183c56048be0140
gaurdhairya111 <gaur.dhairya111@gmail.com>
2026-05-21 13:59:27 +0530

    check the input number is even or odd

---
+++ week_1/odd_even_checker.py
@@ -0,0 +1,10 @@
+x = int(input("enter the number to be checked here: "))
+
+if x%2==0:
+    print(f"the entered number {x} is an even number")
+
+else :
+    print("The entered number is an odd number")
+
+print("this is all for this program \t" \
+ "by!")
